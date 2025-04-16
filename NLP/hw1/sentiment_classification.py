593d37edebf48fc8925449fed097e2d73f8bfb3e
chrisluo5311 <chrislo5311@gmail.com>
2025-04-15 18:47:37 -0700

    update accuracy to 0.9229166666666667 & add symspellpy

--- NLP/hw1/sentiment_classification.py
+++ NLP/hw1/sentiment_classification.py
@@ -5,6 +5,17 @@ import numpy as np
 from nltk.stem import PorterStemmer
 from nltk.tokenize import word_tokenize
 from sklearn.feature_extraction.text import TfidfVectorizer
+from symspellpy import SymSpell, Verbosity
+
+# init SymSpell
+symSpell = SymSpell(max_dictionary_edit_distance=2, prefix_length=7)
+symSpell.load_dictionary("./frequency_dictionary_en_82_765.txt", term_index=0, count_index=1)
+
+def correct_mispronounciation(text):
+    possible_answer = symSpell.lookup(text, Verbosity.CLOSEST, max_edit_distance=2)
+    if possible_answer:
+        return possible_answer[0].term
+    return text
 
 def import_csv(file_path, is_label=False):
     document = []
@@ -18,19 +29,22 @@ def import_csv(file_path, is_label=False):
                     label = int(each_line[0].strip())
                     document.append(label)
                 else:
-                    # text = each_line[0].strip() + " "
+                    # text = each_line[0].strip() + " " # append brand name to text
                     text = each_line[1].strip()
-                    text = text.lower()
+                    text = text.lower() # lower case
                     text = re.sub(r"@\w+", '', text) # remove @
                     text = re.sub(r"#\w+", '', text) # remove #
                     text = re.sub(r'\d+', '', text) # remove numbers
                     text = re.sub(r'[^a-zA-Z0-9\s]', " ", text) # remove special characters
                     text = re.sub(r'\s+', " ", text).strip() # remove extra spaces
-
                     tokens = word_tokenize(text)
-                    after_stem = [stemmer.stem(each_word) for each_word in tokens]
-                    text = " ".join(after_stem)
-                    document.append(text)
+                    # correct mispronounciation
+                    corrected_tokens = [correct_mispronounciation(word) for word in tokens]
+                    # word stemming
+                    after_stem = [stemmer.stem(each_word) for each_word in corrected_tokens]
+                    cleaner_text = " ".join(after_stem)
+                    # print(cleaner_text)
+                    document.append(cleaner_text)
     return document
 
 def import_weight_csv(file_path):
@@ -65,7 +79,6 @@ def convert_data_to_tfidf(doc, vectorizer, is_test=False):
         X_test = np.hstack([test_tfidf_np, bias])
         return X_test
 
-
 def sigmoid(r):
     return 1 / (1 + np.exp(-r))
 
@@ -83,7 +96,7 @@ def monitor_loss(y_train, y_predict):
 def monitor_data_likelihood(y_train, y_predict):
     return np.mean(y_train * np.log(y_predict) + (1 - y_train) * np.log(1 - y_predict))
 
-# best 30000
+# gradient ascent but monitor cross entropy loss & data likelihood during training
 def fit_gd(X_train, y_train, eta=0.01, n_iters=20000, epsilon=1e-6):
     print("Start fitting...")
     # weight = np.random.randn(X_train.shape[1], 1) * 0.01
@@ -91,7 +104,6 @@ def fit_gd(X_train, y_train, eta=0.01, n_iters=20000, epsilon=1e-6):
     cur_iter = 1
     prev_loss = float('inf')
     while cur_iter < n_iters:
-        cur_iter += 1
         y_pred = sigmoid(X_train.dot(weight))
         gradient = X_train.T.dot(y_train - y_pred)
         weight += eta * gradient
@@ -103,12 +115,14 @@ def fit_gd(X_train, y_train, eta=0.01, n_iters=20000, epsilon=1e-6):
             # eta /= 2
             # print(f"New eta: {eta:.6f}")
         if abs(prev_loss - cur_loss) < epsilon:
-            print(f"Break at iteration {cur_iter} Current Loss: {cur_loss:.6f} Previous Loss: {prev_loss:.6f}")
+            print(f"Early Break at iteration {cur_iter} Current Loss: {cur_loss:.6f} Previous Loss: {prev_loss:.6f}")
             break
         if cur_loss < prev_loss:
             prev_loss = cur_loss
+        cur_iter += 1
     return weight
 
+# Not using ❗️
 def fit_stochastic_gd(X_train, y_train, eta=0.1, n_iters=30000, epsilon=1e-5, batch_size=32):
     tolearance = 10
     weight = np.random.randn(X_train.shape[1], 1) * 0.01
@@ -201,6 +215,7 @@ def output_test_file(x_test_document, ultra_weight, file_path, vectorizer):
     print(f"Test data shape: {X_test.shape}")
     s_val = sigmoid(X_test.dot(ultra_weight))
     np.savetxt(file_path, s_val, fmt='%.6f')
+    print(f"Writing file successfully to {file_path} !!!")
 
 if __name__ == '__main__':
     x_train_path = "./train_data/x_train.csv"
@@ -212,33 +227,17 @@ if __name__ == '__main__':
 
     x_test_doc = import_csv(x_test_path) # len: 600
 
-    # there are total 4510 features
-    # best_mse_final = float('inf')
-    # best_ft_cnt = 0
-    # best_accuracy = float('-inf')
-    # for ft_cnt in range(100,4510,100):
-    #     print(f"Current Feature Count: {ft_cnt}")
-    #     X_train = convert_data_to_tfidf(x_document,ft_cnt)
-    #     y_train = numpy.array(y_label).reshape(-1, 1)
-    #
-    #     # first inner test by splitting the data
-    #     tmp_MSE, tmp_weight = inner_split_train_test(X_train, y_train, ft_cnt)
-    #     train_pred = predict(X_train, tmp_weight)
-    #     tmp_acc = accuracy(train_pred, y_train)
-    #     if tmp_acc > best_accuracy:
-    #         best_mse_final = tmp_MSE
-    #         best_ft_cnt = ft_cnt
-    #         best_accuracy = tmp_acc
-    #         write_weight(tmp_weight, "./weight_history", "best_weight", best_mse_final, best_ft_cnt)
-    #
-    # print("Best MSE: ", best_mse_final)
-    # print("Best Feature Count: ", best_ft_cnt)
-    # print("Best Accuracy: ", best_accuracy)
-
     # with ngram_range=(1,2), max_feature: 8000, sublinear_tf= False or True => accuracy: 0.91125
     # with ngram_range=(1,2), max_feature: 1000, sublinear_tf= False or True => accuracy: 0.9145833333333333
-    ft_cnt = 1000
+    # with ngram_range=(1,2), max_feature: 1000, sublinear_tf= False or True => accuracy: 0.91875
+    # with ngram_range=(1,2), max_feature: 1100, sublinear_tf= False or True => accuracy: 0.9191666666666667
+    # with ngram_range=(1,2), max_feature: 1200, sublinear_tf= False or True => accuracy: 0.9229166666666667
     # ft_cnt = None # 0.90375
+    ft_cnt = 1200
+    # best_training_accuracy = 0.9229166666666667
+    # best_max_ft = -1
+    # for i in range(1200,1400,200):
+        # print(f"max_features: {i}")
     vectorizer = TfidfVectorizer(max_features=ft_cnt, ngram_range=(1,2), sublinear_tf=True)
     X_train = convert_data_to_tfidf(x_document, vectorizer)
     y_train = numpy.array(y_label).reshape(-1, 1)
@@ -246,10 +245,18 @@ if __name__ == '__main__':
     # first inner test by splitting the data
     best_mse, best_weight = inner_split_train_test(X_train, y_train, ft_cnt)
     train_pred = predict(X_train, best_weight)
-    print("Training Accuracy:", accuracy(train_pred, y_train))
-    # write_weight(best_weight, "./weight_history", "final_ultra_best_weight", best_mse, ft_cnt)
+    cur_accuracy = accuracy(train_pred, y_train)
+    print(f"Training Accuracy: {cur_accuracy}")
+        # if cur_accuracy > best_training_accuracy:
+        #     best_training_accuracy = cur_accuracy
+        #     best_max_ft = i
+    # print(f"Best Training Accuracy: {best_training_accuracy}")
+    # print(f"Best max_features: {best_max_ft}")
 
-    # Final: output the test result
-    # best_weight = np.array(import_weight_csv("./weight_history/final_ultra_best_weight_0.4083_1000.csv.csv")).astype(float)
+    # 🗂️ write weight to file
+    # write_weight(best_weight, "./weight_history", "finalV2_ultra_best_weight", best_mse, ft_cnt)
+
+    # 🗂️ write test file "yprob_test.txt"
+    # best_weight = np.array(import_weight_csv("./weight_history/.csv")).astype(float)
     # file_path_for_test = "predict_test/yprob_test.txt"
     # output_test_file(x_test_doc, best_weight, file_path_for_test, vectorizer)
--- NLP/hw1/test/test_small_function.py
+++ NLP/hw1/test/test_small_function.py
@@ -1,4 +1,6 @@
 import numpy as np
+from textblob import TextBlob
+from symspellpy import SymSpell, Verbosity
 
 def euclidean_distance(a, b):
     return np.linalg.norm(a-b)
@@ -8,7 +10,31 @@ if __name__ == '__main__':
     # b = np.array([0,-1,2])
     # print(euclidean_distance(a, b))
 
-    yprob_test = np.loadtxt('../predict_test/yprob_test_old.txt')
-    print(yprob_test)
-    print(yprob_test.shape)
-    print(type(yprob_test))
+    # yprob_test = np.loadtxt('../predict_test/yprob_test_oldest.txt')
+    # print(yprob_test)
+    # print(yprob_test.shape)
+    # print(type(yprob_test))
+
+    # correct_str = str(TextBlob(wrong_str).correct())
+    # print(correct_str)
+
+    # init SymSpell
+    symSpell = SymSpell(max_dictionary_edit_distance=2, prefix_length=7)
+    symSpell.load_dictionary("../frequency_dictionary_en_82_765.txt", term_index=0, count_index=1)
+
+    # input text
+    wrong_str_list = ["Absolutel junk",
+                      "This is a simple little phone to use, but the breakage is unacceptible.",
+                      "This is so embarassing and also my ears hurt if I try to push the ear plug into my ear."]
+
+    # tokenize and correct each word
+    for line in wrong_str_list:
+        corrected_str = []
+        for word in line.split():
+            suggestions = symSpell.lookup(word, Verbosity.CLOSEST, max_edit_distance=2)
+            if suggestions:
+                corrected_str.append(suggestions[0].term)
+            else:
+                corrected_str.append(word)
+        corrected_text = ' '.join(corrected_str)
+        print(corrected_text)
