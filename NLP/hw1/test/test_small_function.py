260eaeb9483d65f91fb2b15ea7568469ee5364f0
chrisluo5311 <chrislo5311@gmail.com>
2025-04-15 18:54:07 -0700

    update SymSpell test function

--- NLP/hw1/test/test_small_function.py
+++ NLP/hw1/test/test_small_function.py
@@ -4,27 +4,15 @@ from symspellpy import SymSpell, Verbosity
 def euclidean_distance(a, b):
     return np.linalg.norm(a-b)
 
-if __name__ == '__main__':
-    # a = np.array([1,3,5])
-    # b = np.array([0,-1,2])
-    # print(euclidean_distance(a, b))
-
-    # yprob_test = np.loadtxt('../predict_test/yprob_test_oldest.txt')
-    # print(yprob_test)
-    # print(yprob_test.shape)
-    # print(type(yprob_test))
-
-    # correct_str = str(TextBlob(wrong_str).correct())
-    # print(correct_str)
-
+def correct_words():
     # init SymSpell
     symSpell = SymSpell(max_dictionary_edit_distance=2, prefix_length=7)
     symSpell.load_dictionary("../frequency_dictionary_en_82_765.txt", term_index=0, count_index=1)
 
     # input text
     wrong_str_list = ["Absolutel junk",
-                      "This is a simple little phone to use, but the breakage is unacceptible.",
-                      "This is so embarassing and also my ears hurt if I try to push the ear plug into my ear."]
+                    "This is a simple little phone to use, but the breakage is unacceptible.",
+                    "This is so embarassing and also my ears hurt if I try to push the ear plug into my ear."]
 
     # tokenize and correct each word
     for line in wrong_str_list:
@@ -37,3 +25,18 @@ if __name__ == '__main__':
                 corrected_str.append(word)
         corrected_text = ' '.join(corrected_str)
         print(corrected_text)
+
+if __name__ == '__main__':
+    # a = np.array([1,3,5])
+    # b = np.array([0,-1,2])
+    # print(euclidean_distance(a, b))
+
+    yprob_test = np.loadtxt('../predict_test/yprob_test.txt')
+    print(yprob_test)
+    print(yprob_test.shape)
+    print(type(yprob_test))
+
+    # correct_str = str(TextBlob(wrong_str).correct())
+    # print(correct_str)
+
+    # correct_words()
